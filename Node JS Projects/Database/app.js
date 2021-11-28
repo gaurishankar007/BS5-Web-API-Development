e45036b6b178cc5ae95354f21fb684ec2c9961dc
gaurishankar007 <57608187+gaurishankar007@users.noreply.github.com>
2021-11-28 11:59:21 +0545

    Updating the directory.

--- Node JS Projects/Database/app.js
+++ Node JS Projects/Database/app.js
@@ -4,54 +4,76 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-mongoose.connect('mongodb://127.0.0.1:27017/first-node-mangoDB', {
+mongoose.connect('mongodb://127.0.0.1:27017/first-node-mangodb', {
     useNewUrlParser: true,
     useUnifiedTopology : true
 });
 
-// const user = mongoose.model('User', {
-//     username: {
-//         type: String
-//     },
-//     phone: {
-//         type: String
-//     },
-//     address: {
-//         type: String
-//     }
-// });
+const user = mongoose.model('User', {
+    username: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    address: {
+        type: String
+    }
+});
 
-// const userData = new user({
-//     username: "Gauri",
-//     phone: "9816346714",
-//     address: "Morang, Sundarharaincha-1",
-// });
+const product = mongoose.model('Product', {
+    pName: {
+        type: String
+    },
+    pPrice: {
+        type: String
+    },
+    pColor: {
+        type: String
+    }
+});
 
-// userData.save();
+/*
+const userData = new user({
+    username: "Gauri",
+    phone: "9816346714",
+    address: "Morang, Sundarharaincha-1",
+});
 
-// const product = mongoose.model("Product", {
-//     pName: {
-//         type: String
-//     },
-//     pPrice: {
-//         type: String
-//     },
-//     pColor: {
-//         type: String
-//     }
-// });
+userData.save();
 
-// const productData  = new product({
-//     pName: "Marker",
-//     pPrice: "$10",
-//     pColor: "Black"
-// });
+const product = mongoose.model("Product", {
+    pName: {
+        type: String
+    },
+    pPrice: {
+        type: String
+    },
+    pColor: {
+        type: String
+    }
+});
 
-// productData.save();
+const productData  = new product({
+    pName: "Marker",
+    pPrice: "$10",
+    pColor: "Black"
+});
+
+productData.save();
+*/
  
 app.post("/product/insert", function(req, res){
-    res.send("test test test");
-    console.log("Username: " + req.body.username+ ", Address: "+ req.body.address);
+    res.send("Hello there! new product has been added.");
+    const productData = new product(req.body);
+    productData.save();
 });
+
+// app.post("/user/insert", function(req, res) {
+//     res.send("Hello there! new user has been added.")
+//    console.log("Username: " + req.body.pName+ ", Address: "+ req.body.address);
+//     const userData = new user(req.body);
+//     userData.save();
+// });
 
 app.listen(90);
