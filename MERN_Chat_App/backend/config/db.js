e99c87f670f8ab977bd5e56516bd981d876c4eb8
gaurishankar007 <57608187+gaurishankar007@users.noreply.github.com>
2021-12-16 16:56:18 +0545

    Removing node_modules

--- MERN_Chat_App/backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = mongoose.connect('mongodb://127.0.0.1:27017/ChatApp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-
-module.exports = connectDB;
