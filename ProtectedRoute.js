9c9bfa03769a28ec269db7e7cc5ba3739dfe3dc9
gaurishankar007 <57608187+gaurishankar007@users.noreply.github.com>
2022-02-11 00:33:38 +0545

    Addition information assignment

---
+++ ProtectedRoute.js
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+
+
+function PrivateRoute({ children }) {
+    var isAuth;
+  if(localStorage.getItem('tok')){
+      isAuth= true
+  }
+  else{
+      isAuth= false
+  }
+  return isAuth ? children : <Navigate to="/login" />;
+}
+
+export default PrivateRoute;
